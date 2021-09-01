16325c8c4627085a4c2abdde1d132ff4007bee98
Mohammed Nadeem Shareef <nadeemshareef934@gmail.com>
2021-09-01 19:19:20 +0530

    Program to copy data of one file to another file

---
+++ ReadAndWrite.java
@@ -0,0 +1,31 @@
+import java.io.*;
+class ReadAndWrite {
+    public static void main(String args[]) {
+        FileReader fr = null;
+        FileWriter fw = null;
+        try {
+            fr = new FileReader("exampleFileRead.txt");
+            fw = new FileWriter("exampleFileWrite.txt");
+            int c = fr.read();
+            while (c != -1) {
+                fw.write(c);
+                c = fr.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            close(fr);
+            close(fw);
+        }
+    }
+
+    public static void close(Closeable stream) {
+        try {
+            if (stream != null) {
+                stream.close();
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
