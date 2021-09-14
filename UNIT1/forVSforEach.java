1eb999443683def912afe26cd795427e07def20b
shareef <nadeemshareef934@gmail.com>
2021-09-14 19:50:31 +0530

    🙌 add for vs for-each program

---
+++ UNIT1/forVSforEach.java
@@ -0,0 +1,22 @@
+package UNIT1;
+
+public class forVSforEach {
+    public static void main(String[] args) {
+        // Adding N numbers with regular for loop.
+        int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int sum = 0;
+        int sumWithForEach = 0;
+        // Regular For loop
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println("Sum is: " + sum);
+
+        // For Each loop
+        for (int x : numbers) {
+            sumWithForEach += x;
+        }
+        System.out.println("Sum is: " + sumWithForEach);
+
+    }
+}
