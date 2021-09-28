89fadf59e0b54b761614038a69a5bcb90576f51c
shareef <nadeemshareef934@gmail.com>
2021-09-28 17:52:15 +0530

    🚀 Add 2 new programs

---
+++ UNIT3/CustomException.java
@@ -0,0 +1,26 @@
+package UNIT3;
+
+class GreaterThanTen extends Exception {
+    public String toString() {
+        return "Number is greater than 10";
+    }
+}
+
+public class CustomException {
+    static void compute(int a) throws GreaterThanTen {
+        System.out.println("Given number: " + a);
+
+        if (a > 10) {
+            throw new GreaterThanTen();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            compute(1);
+            compute(20);
+        } catch (GreaterThanTen e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ UNIT3/Finally.java
@@ -0,0 +1,16 @@
+// package UNIT3;
+
+// public class Finally {
+
+// static void procA() {
+// try {
+
+// } catch (Exception e) {
+// //TODO: handle exception
+// }
+// }
+
+// public static void main(String[] args) {
+
+// }
+// }
