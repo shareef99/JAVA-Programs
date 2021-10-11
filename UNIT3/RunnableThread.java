877dcac36d6ac3d8a10a95ded437f628e0551681
shareef <nadeemshareef934@gmail.com>
2021-10-11 13:17:56 +0530

    🚀 Add runnable and thread programs

---
+++ UNIT3/RunnableThread.java
@@ -0,0 +1,59 @@
+package UNIT3;
+
+/**
+ * Q1) What are the different ways of creating a thread? Explain any one method
+ * with example. (3-times) (Runnable Example)
+ */
+
+class NewThread implements Runnable {
+    Thread t;
+
+    NewThread() {
+        // Create a new, second thread
+        t = new Thread(this, "Demo Thread");
+        System.out.println("Child thread: " + t);
+        t.start(); // Start the thread
+    }
+
+    // This is the entry point for the second thread.
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Child Thread: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Child interrupted.");
+        }
+        System.out.println("Exiting child thread.");
+    }
+}
+
+public class RunnableThread {
+    public static void main(String[] args) {
+        new NewThread(); // create a new thread
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Main Thread: " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted.");
+        }
+        System.out.println("Main thread exiting.");
+    }
+}
+
+// Child thread: Thread[Demo Thread,5,main]
+// Main Thread: 5
+// Child Thread: 5
+// Child Thread: 4
+// Main Thread: 4
+// Child Thread: 3
+// Child Thread: 2
+// Main Thread: 3
+// Child Thread: 1
+// Exiting child thread.
+// Main Thread: 2
+// Main Thread: 1
+// Main thread exiting.
